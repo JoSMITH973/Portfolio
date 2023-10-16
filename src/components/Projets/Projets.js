150df5286fa93427026d6e6d50ed9d1046c42aef
JoSMITH973 <72966617+JoSMITH973@users.noreply.github.com>
2023-10-17 01:20:50 +0200

    Ajout slider dans la partie projet

--- src/components/Projets/Projets.js
+++ src/components/Projets/Projets.js
@@ -1,41 +1,61 @@
 import './Projets.css'
 import data from '../../data/projets.json'
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+// thème par défaut de react splide
+import '@splidejs/react-splide/css';
+import '@splidejs/react-splide/css/skyblue';
+// import '@splidejs/react-splide/css/sea-green';
+import { useEffect, useState } from 'react';
 
 function Projets() {
+    
+    // On définit les options à rajouter ou retirer au slider selon l'orientation de l'appareil
+    const [orientationOption, setOrientationOption] = useState([]);
+    
+    useEffect(()=> {
+
+        async function handleResize() {
+            let orientation = window.innerHeight > window.innerWidth ? "portrait" : "paysage";
+            console.log('orientation :', orientation);
+            switch (orientation) {
+                case "portrait":
+                    setOrientationOption([{type: "slide", height: null, width: "100vw"}]);  
+                    break;
+                    
+                case "paysage":
+                    setOrientationOption([{type: "loop", height: "75vh", width: null}]);
+                    break;
+            }
+        }
+    
+        // Attach the event listener to the window object
+        window.addEventListener('resize', handleResize);
+    
+        // Remove the event listener when the component unmounts
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [])
+
 	return (
 		<div data-aos="fade-up" className="proj">
-		{/* <div className="portfolio">
-		<div className="portfolio-container"> */}
 			<h1 className='comp-title'>Projets</h1>
-            <div className='proj-group'>
-            {/* <div className='portfolio-item'> */}
+            <Splide 
+                options={{
+                    rewind: true,
+                    gap   : '1rem',
+                    padding: '5rem',
+                    ...orientationOption[0],
+                }} 
+                className='splide-tag'
+                aria-label="Projets"
+            >
                 {data.sort((a, b) => (a.id > b.id ? 1 : -1)).map((proj)=> (
-                    <div className='proj-card'>
-                        <div className='card-title' style={{color:proj.color}}>
-                            {proj.nom}
-                        </div>
-                        <img className='card-img' src={process.env.PUBLIC_URL + (proj.main_image ||'images/background_competences.webp')} />
-                    </div>
-                    // <div className='portfolio-wrap'>
-                    //     <div className='portfolio-info'>
-                    //         <h4>
-                    //             {proj.nom}
-                    //         </h4>
-                    //         <p>
-                    //             {proj.type}
-                    //         </p>
-                    //     </div>
-                    //     <img className='img-fluid' src={process.env.PUBLIC_URL + (proj.main_image ||'images/background_competences.webp')} />
-                    //     {/* <img className='card-img' src={process.env.PUBLIC_URL + proj.main_image} /> */}
-                    // </div>
+                    <SplideSlide key={proj.id}>
+                        <img className='splide-img' src={process.env.PUBLIC_URL + (proj.main_image ||'images/background_competences.webp')} alt={proj.main_image}/>
+                    </SplideSlide>
                 ))}
-                {/* <ul>
-                    <li>DS exotique : Blog culinaire</li>
-                    <li>SVentory : Application de gestion d'inventaire</li>
-                    <li>Your Favorites Simpsons : Projets scolaire...</li>
-                    <li>Galcktickos CB : Projets scolaire...</li>
-                </ul> */}
-            </div>
+            </Splide>
 		</div>
 	);
 }
