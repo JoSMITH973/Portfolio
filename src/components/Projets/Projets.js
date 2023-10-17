6752e09208f31f10a3f8c4f144ce0ac73eacf4f5
JoSMITH973 <72966617+JoSMITH973@users.noreply.github.com>
2023-10-18 01:01:21 +0200

    clean dossier build

--- src/components/Projets/Projets.js
+++ src/components/Projets/Projets.js
@@ -2,23 +2,14 @@ import './Projets.css'
 import data from '../../data/projets.json'
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 // thème par défaut de react splide
-import '@splidejs/react-splide/css';
 import '@splidejs/react-splide/css/sea-green';
 import { useEffect, useState } from 'react';
 
 function Projets() {
     // On définit les options à rajouter ou retirer au slider selon l'orientation de l'appareil
     const [options, setOptions] = useState({});
-    const [orientationImg, setOrientationImg] = useState(null);
     
     useEffect(()=> {
-        // const getImg = async () => {
-        //     const dd = await orientationImg(process.env.PUBLIC_URL + data[3].main_image)
-        //     return dd;
-        // }
-        // // getImg()
-        // console.log("path :", process.env.PUBLIC_URL + data[3].main_image)
-        // console.log("getOrientationImg :", getImg());
         
         async function handleResize() {
             let orientation = window.innerHeight > window.innerWidth ? "portrait" : "paysage";
@@ -28,7 +19,6 @@ function Projets() {
 						{ 
                             type: "slide",
                             height: null,
-                            // autoHeight: true,
                             width: "100vw",
                             heightRatio: 0.5,
                             gap   : null,
@@ -75,8 +65,9 @@ function Projets() {
                 aria-label="Projets"
             >
                 {data.sort((a, b) => (a.id > b.id ? 1 : -1)).map((proj)=> (
-                    <SplideSlide className={proj.orientation === "portrait" ? "slide-portrait" : ""} key={proj.id}>
+                    <SplideSlide className={proj.orientation === "portrait" ? "slide-portrait" : "slide"} key={proj.id}>
                         <img className='splide-img' src={process.env.PUBLIC_URL + (proj.main_image ||'images/background_competences.webp')} alt={proj.main_image}/>
+                        <p className='splide-nom'>{proj.nom}</p>
                     </SplideSlide>
                 ))}
             </Splide>
